2f016960db9825b63e7b891d9e43247c3c529279
Shad150 <cgpablo150@gmail.com>
2020-03-22 14:50:04 +0100

    Initial commit

---
+++ Assets/Ejercicio1/Scripts/Ejericio1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejericio1 : MonoBehaviour
+{
+    GameObject prefabObstacle;
+    int n;
+
+    //a)
+
+    GameObject SpawnObstacle()
+    {
+        GameObject obstacle = Instantiate(prefabObstacle);    
+
+        return obstacle;
+    }
+
+    //b)
+
+    void SpawnObstacle2()
+    {
+        Instantiate(prefabObstacle);
+    }
+
+    //c)
+
+    void Starter()
+    {
+        int x = 5;
+        int y = 3;
+
+        Vector2 position;
+        position.x = x;
+        position.y = y;
+
+        SpawnObstacleIn(position);
+    }
+
+    void SpawnObstacleIn(Vector2 position)
+    {
+        Instantiate(prefabObstacle, position, Quaternion.identity);
+    }
+
+    //d)
+
+    void Start()
+    {
+        SpawnObstacles(5);
+    }
+
+    void SpawnObstacles(int number)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            Instantiate(prefabObstacle);
+        }
+    }
+}
---
+++ Assets/Ejercicio2/Scripts/Ejercicio2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    
+    int n = 4;
+    float time = 10;
+
+    public GameObject frasesGO;
+    public Text frases;
+
+    //a)
+    List<string> frasesMotivacionales = new List<string>() { "Javi's not dead (yet)", "Antivaxers gonna die after this", "You survived NA classes, then you can survive this", "We have vacations, yay ", "We can continue the Roguelike" };        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //b)
+        for (int i = 0; i <= n; i++)                        
+        {
+            Debug.Log(frasesMotivacionales[i]);
+        }
+
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        frases.text = time.ToString();
+
+        while (time < 10)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                frases.text = frasesMotivacionales[i];
+            }
+            if (time > 10)
+            {
+                time = 0;
+            }
+        }
+        
+
+    }
+}
